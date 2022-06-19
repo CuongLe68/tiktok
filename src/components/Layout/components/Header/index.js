bacd4de0935812fa2e3dd11294f208b0df35df4b
Cuong Le <nguyenvanminh8864@gmail.com>
2022-06-20 02:21:47 +0700

    Xay dung phan popper khi nhap vao o tim kiem

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,10 @@
 import classNames from "classnames/bind";
+import Tippy from "@tippyjs/react/headless"; //khi hover vào mặc định sẽ hiển thị cái gì đó
 import styles from "./Header.module.scss";
 import images from "~/assets/images";
+import { Wrapper as PopperWrapper } from "~/components/Popper";
 
+//import icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCircleXmark,
@@ -9,25 +12,60 @@ import {
   faMagnifyingGlass,
 } from "@fortawesome/free-solid-svg-icons";
 
+//import use react
+import { useEffect, useState } from "react";
+import AccountItem from "~/components/AccountItem";
+
 const cx = classNames.bind(styles); // giúp class có thể viết kiểu a-b (vd: header-item)
 
 function Header() {
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResult([]);
+    }, 0);
+  }, []);
+
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
         <div className={cx("logo")}>
           <img src={images.logo} alt="Logo Tiktok" />
         </div>
-        <div className={cx("search")}>
-          <input placeholder="Search accounts and videos" spellCheck={false} />
-          <button className={cx("clear")}>
-            <FontAwesomeIcon icon={faCircleXmark} />
-          </button>
-          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
-          <button className={cx("search-btn")}>
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </button>
-        </div>
+
+        <Tippy
+          interactive //Giúp tương tác được với phần value của tippy
+          visible={searchResult.length > 0}
+          render={(attrs) => (
+            <div className={cx("search-result")} tabIndex="-1" {...attrs}>
+              <PopperWrapper>
+                <h4 className={cx("search-title")}>Account</h4>
+
+                {/* Hiện account */}
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+              </PopperWrapper>
+            </div>
+          )}
+        >
+          <div className={cx("search")}>
+            <input
+              placeholder="Search accounts and videos"
+              spellCheck={false}
+            />
+            <button className={cx("clear")}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+            <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+
+            <button className={cx("search-btn")}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+        </Tippy>
+
         <div className={cx("acctions")}></div>
       </div>
     </header>
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,15 @@
 import { HeaderOnly } from "~/components/Layout";
 
 //Pages
-import Home from "~/pages/Home";
 import Following from "~/pages/Following";
+import Home from "~/pages/Home";
 import Profile from "~/pages/Profile";
 import Upload from "~/pages/Upload";
 import Search from "~/pages/Search";
 
 const publicRoutes = [
-  { path: "/", component: Home },
   { path: "/following", component: Following },
+  { path: "/", component: Home },
   { path: "/profile", component: Profile },
   { path: "/upload", component: Upload, layout: HeaderOnly },
   { path: "/search", component: Search, layout: null },
